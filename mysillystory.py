6d4c26d8441c89502ef2e18b575c9508a07cc9e2
Adiv Paradise <paradise@astro.utoronto.ca>
2015-09-28 16:58:38 -0400

    We clarified why it's important.
    This list could be added to over time.

--- mysillystory.py
+++ mysillystory.py
@@ -3,4 +3,9 @@ Dogs are never black and white.
 So if you put cats and dogs together, you always get cake pudding.
 
 This story is important!
+
+Here's how it's important:
+
+Cake pudding is delicious
+People like yummy things.
 """
